47aa3db7f826845ba03562179ad24835d915c027
Emily Carlisle <emilydeluna96@gmail.com>
2020-07-18 12:46:34 -0500

    adding html routes to render

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const router = require("./apiRoutes");
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+router.get("/", (req, res) => {
+    res.sendFile(path.joing(__dirname, '../public/index.html'));
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+
+module.exports = router;
